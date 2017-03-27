83b6c18fb4bdae8d8402502bb61e8c26a68e4637
fingerman <adamov.george@gmail.com>
2017-03-27 17:27:32 +0300

    complete Model: NSI.html

--- skriptNSI.R
+++ skriptNSI.R
@@ -1,9 +1,6 @@
-##  NSI SKRIPT
-
-
+# load  data 
 rm(list = ls())
-getwd()
-#setwd("C:/Users/computer/Desktop/data")
+setwd("C:/Users/computer/Desktop/data") # dir
 library(Hmisc)
 library(fields)
 library(devtools)  #source_gist(4676064)
@@ -11,36 +8,73 @@ library(devtools)  #source_gist(4676064)
 
 Sys.setlocale("LC_CTYPE", "russian")
 data08 = read.csv("EHIS_BG_2008_anon.csv")
-class(data08)
-save.image(file = "data08.RData")
-load("data08.RData")
-
-
 row.names(data08) <- NULL
 data08 <- data08[,-(1)]  # drop unused index
+save.image(file = "data.RData")
+load("data.RData")
+keep columns < 500 NA
+data <- data[colSums(is.na(data)) < 500]
+
+# t(stats(data))  # check missings
+
+## rename variables
+data$health <- data$HS01      # health status
+data$age <- data$AgeGroup_85  # age
+data$edu <- data$HH07_gr      # education
+data$hshold <- data$HHACT_gr  # household size
+data$income <- data$IN05      # income
+data$smoker <- data$SK01      # smoker
+data$attGood <- data$SF04      # life attitude
+data$feelTired <- data$SF08   # general exhaustion
+data$female <- data$SEX
 
 
 
 
-#handle missings
-t(stats(data))
-utils::View(data)
-describe(data08)
-names(data08)
+
+data<-subset(data, !(health>5))
+table(data$female)
+table(data$health)
+table(data$age)
+table(data$hshold)
+table(data$income)
+table(data$smoker)  # delete 9
+data<-subset(data, !(smoker>8))
+table(data$attGood)  # delete 8, 9
+data<-subset(data, !(attGood>7))
+table(data$feelTired)  # delete 8, 9
+data<-subset(data, !(feelTired>7))
+data<-subset(data, !(edu>7))
 
 
 
 
+# recode
 
-dataValid <- data08[colSums(is.na(data08)) < 500]
-t(stats(dataValid))
-str(dataValid)
-View(dataValid)
-rm(dataValid)
-cor(dataValid)
-cor(dataValid, use = "pairwise.complete.obs")
+data$female <-ifelse(data$female==2,1,0) # female = 1, male = 0
+data$female<-factor(data$female)
 
 
 
+data$health <- mapvalues(data$health, from = c("1", "2", "4", "5"), to = c("5", "4", "2", "1"))
+data$logHealth <- log(data$health)
+
+
+
+# correlations
+
+data$health <- as.numeric(data$health)
+data$femaleNum <- as.numeric(data$female)
+
+round(cor(data[c("health", "femaleNum", "age", "income", "hshold", "smoker", "attGood", "feelTired")], 
+          use="pairwise.complete.obs"), 2)
+
+
+View(data)
+table(data$health)
+class(data$health)
+data$health <- as.numeric(data$health)
+data$femaleNum <- as.numeric(data$female)
+rm(fit)
 
 
